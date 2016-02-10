88305f61a2451b0e2e5b78dcb99d9c5e7771673f
Fredrik Haglund <mejl@fredrikhaglund.se>
2016-02-10 16:29:00 +0100

    Initial commit.

---
+++ Uppgift1/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Uppgift1.Models;
+
+namespace Uppgift1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var model = new QuizViewModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(QuizViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", model);
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Uppgift1/Models/QuizViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Uppgift1.Models
+{
+    public class QuizViewModel
+    {
+        [Required(ErrorMessage = "Skriv ditt namn")]
+        public string Namn { get; set; }
+
+        [Required(ErrorMessage = "Skriv din ålder")]
+        [Range(13, 99, ErrorMessage = "Ålder mellan 13 och 99 år")]
+        public int? Age { get; set; }
+
+        [Required(ErrorMessage = "Välj din favoritårstid")]
+        public string FavoriteSeason { get; set; }
+
+        [Required(ErrorMessage = "Välj ditt favoritgodis")]
+        public string FavoriteCandy { get; set; }
+
+        public string[] SeasonList = { "Vår", "Sommar", "Höst", "Vinter"};
+        public string[] CandyList = { "Center", "Colaflaskor", "Plopp", "Gelehallon" };
+    }
+}
